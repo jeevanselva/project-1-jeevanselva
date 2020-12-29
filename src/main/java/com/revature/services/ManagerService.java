3783c98c35f300ccb2ab0a12c737792a5a0dd3d9
jeevanselva <jeevan.selva@outlook.com>
2020-12-28 20:42:24 -0500

    Update EmployeeService and ManagerService

--- src/main/java/com/revature/services/ManagerService.java
+++ src/main/java/com/revature/services/ManagerService.java
@@ -1,5 +1,23 @@
 package com.revature.services;
 
+import com.revature.models.ListOfReimbursements;
+import com.revature.models.Reimbursement;
+import com.revature.repositories.ManagerDAO;
+
 public class ManagerService {
+	ManagerDAO dao;
+
+	public ManagerService() {
+		this.dao = new ManagerDAO();
+	}
+
+	public ListOfReimbursements getAllReimbursements() {
+		ListOfReimbursements list = dao.readEmployeeReimbursements();
+		return list;
+	}
+
+	public void updateReimbursement(Reimbursement reimbursement) {
+		dao.updateReimbursementStatus(reimbursement);
+	}
 
 }
--- src/main/webapp/views/login.js
+++ src/main/webapp/views/login.js
@@ -1,7 +1,7 @@
 async function loginSubmission(e){
     e.preventDefault();
-    let userName = document.getElementById("username-input").value
-    let password = document.getElementById("password-input").value
+    let userName = document.getElementById("inputUserName").value
+    let password = document.getElementById("inputPassword").value
 
     const credentials = {
         username:userName,
